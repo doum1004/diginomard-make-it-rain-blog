e7dabf7fae2a091f66f53562d6c849a3a6cd749e
JEHYEOK LEE <doum1004@gmail.com>
2024-01-12 13:38:02 -0500

    add move_mouse

--- work_toolkit/mouse.py
+++ work_toolkit/mouse.py
@@ -1,5 +1,14 @@
+import random
 import time
 import pyautogui
+
+def move_mouse():
+    # move mouse little around
+    #rand x, y
+    rangeVal = 3
+    x = random.randint(-rangeVal, rangeVal)
+    y = random.randint(-rangeVal, rangeVal)
+    pyautogui.moveRel(x, y)
 
 def click_mouse():
     pyautogui.click()
@@ -10,6 +19,7 @@ def click_mouse_sec(pauseSec=60):
         i = 0
         while True:
             click_mouse()
+            #move_mouse()
             print("Mouse clicked! " + str(i) + " times.")
             i += 1                  
             time.sleep(pauseSec)  # Sleep for 1 minute
